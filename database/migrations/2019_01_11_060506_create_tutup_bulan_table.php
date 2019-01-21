22a617e543819f22f7cae3bb20f5bf41cb50a4bc
dsteve98 <32993002+dsteve98@users.noreply.github.com>
2019-01-21 15:31:36 +0700

    Update 2019_01_11_060506_create_tutup_bulan_table.php

--- database/migrations/2019_01_11_060506_create_tutup_bulan_table.php
+++ database/migrations/2019_01_11_060506_create_tutup_bulan_table.php
@@ -17,6 +17,24 @@ class CreateTutupBulanTable extends Migration
             $table->string('periode',10)->primary();
             $table->boolean('istutup')->default('0');
         });
+
+        $data = array(
+            array('periode'=>'Jan2019', 'istutup'=>'0'),
+            array('periode'=>'Feb2019', 'istutup'=>'0'),
+            array('periode'=>'Mar2019', 'istutup'=>'0'),
+            array('periode'=>'Apr2019', 'istutup'=>'0'),
+            array('periode'=>'May2019', 'istutup'=>'0'),
+            array('periode'=>'Jun2019', 'istutup'=>'0'),
+            array('periode'=>'Jul2019', 'istutup'=>'0'),
+            array('periode'=>'Aug2019', 'istutup'=>'0'),
+            array('periode'=>'Sep2019', 'istutup'=>'0'),
+            array('periode'=>'Okt2019', 'istutup'=>'0'),
+            array('periode'=>'Nov2019', 'istutup'=>'0'),
+            array('periode'=>'Dec2019', 'istutup'=>'0')
+        );
+        
+        DB::table('tutup_bulan')->insert($data);
+
     }
 
     /**
